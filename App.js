34118bd53cfd508790ab972c7be9b5dabd9d9f43
Bohdan <48454610+BohdanSol@users.noreply.github.com>
2021-02-03 10:13:43 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Step One</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
